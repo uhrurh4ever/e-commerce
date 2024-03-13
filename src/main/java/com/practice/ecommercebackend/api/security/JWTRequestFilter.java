2ab6e4dcf22244a7022534f5adf949e1120458ec
Stepan Skvortsov <bober7740@gmail.com>
2024-03-13 21:58:20 +0300

    Next one

---
+++ src/main/java/com/practice/ecommercebackend/api/security/JWTRequestFilter.java
@@ -0,0 +1,19 @@
+package com.practice.ecommercebackend.api.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenHeader = request.getHeader("Authorization");
+        filterChain.doFilter(request,response);
+    }
+}
